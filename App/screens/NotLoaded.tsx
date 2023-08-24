346e85d213ca8e52f5f5ddbfe01125941bd7173e
jaydamani <jaydamani567@gmail.com>
2023-08-24 23:57:37 +0530

    feat: improve loading screen

--- App/screens/NotLoaded.tsx
+++ App/screens/NotLoaded.tsx
@@ -1,5 +1,19 @@
+import { PerformanceMeasureView } from '@shopify/react-native-performance';
+import { StyleSheet, View } from 'react-native';
 import { ActivityIndicator } from 'react-native-paper';
 
 export default function NotLoaded() {
-  return <ActivityIndicator animating />;
+  return (
+    <View style={[styles.container, StyleSheet.absoluteFill]}>
+      <ActivityIndicator animating size="large" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    // zIndex: 20
+  },
+});
